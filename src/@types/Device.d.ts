24795872201856ac0cb9eb91f4efa2d0a79afb61
Andre <cavallari@live.com>
2026-02-27 10:11:16 -0300

    chore: add extra Device typings

--- src/@types/Device.d.ts
+++ src/@types/Device.d.ts
@@ -4,17 +4,26 @@ interface DeviceStatus {
 }
 
 interface Device {
-  id: string;
-  name: string;
+  active_time: number;
+  biz_type: number;
   category: string;
+  create_time: number;
   icon: string;
-  ip?: string;
-  lat?: number;
-  lon?: number;
-  local_key?: string;
+  id: string;
+  ip: string;
+  lat: string;
+  local_key: string;
+  lon: string;
+  model: string;
+  name: string;
   online: boolean;
   owner_id: string;
   product_id: string;
   product_name: string;
   status: DeviceStatus[];
+  sub: boolean;
+  time_zone: string;
+  uid: string;
+  update_time: number;
+  uuid: string;
 }
