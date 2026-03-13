7e252f323a7fb9f8525a6477fb56716ea3ea7c8e
Andre Cavallari <cavallari@live.com>
2026-03-13 03:21:10 -0300

    chore: fix controllers code

--- src/app/iot/controllers/page.tsx
+++ src/app/iot/controllers/page.tsx
@@ -22,11 +22,13 @@ export default function Page() {
     <div className="max-w-4xl mx-auto p-4">
       <Title>Controllers</Title>
 
-      <p className="mt-4 text-gray-600">Controllers are hub&apos;s that connects one or more devices and multiple brands</p>
+      <p className="mt-4 text-gray-600">
+        Controllers are hub&apos;s that connects one or more devices and multiple brands
+      </p>
 
       <ul className="grid grid-cols-2 md:grid-cols-4 gap-3 mx-auto mt-8 mb-12">
         {controllers.map((controller) => (
-          <li key={controller.id} className="border border-gray-300 rounded-lg p-4 text-gray-700 shadow-xl">
+          <li key={controller.id.toString()} className="border border-gray-300 rounded-lg p-4 text-gray-700 shadow-xl">
             <h3 className="text-sm font-bold">{controller.name}</h3>
             <p className="text-sm text-gray-500">{controller.type}</p>
           </li>
